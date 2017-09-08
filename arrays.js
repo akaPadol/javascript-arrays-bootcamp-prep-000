fef4e7ab889c27852950e5a4b6fef3ce57060ddd
Antonino Badalamenti <bada.nino95@gmail.com>
2017-09-08 23:47:51 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,42 @@
+chocolateBars = ["snickers",
+"hundred grand",
+"kitkat",
+"skittles"]
+
+function addElementToBeginningOfArray(arrays,element){
+  var result = new Array()
+  result=[element, ...arrays]
+  return result
+}
+function destructivelyAddElementToBeginningOfArray(Arrays,element){
+  Arrays.unshift(element)
+}
+function addElementToEndOfArray(arrays,element){
+  var result = new Array()
+  result=[...arrays, element]
+  return result
+}
+function destructivelyAddElementToEndOfArray(arrays,element){
+  arrays.push(element)
+}
+
+function accessElementInArray(arrays,index){
+  return arrays[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arrays){
+  arrays.shift()
+}
+function removeElementFromBeginningOfArray(arrays){
+  var result = new Array()
+  result = arrays.slice(1)
+  return result
+}
+function destructivelyRemoveElementFromEndOfArray(arrays){
+  arrays.pop()
+}
+function removeElementFromEndOfArray(arrays){
+  var result = new Array()
+  result= arrays.slice(0, arrays.lenght -1)
+  return result
+}
